5d37686702d7113ce281bfdd700427375b54338d
Elmehdi Aitbrahim <eaitbrahim@gmail.com>
2020-12-17 16:08:43 -0500

    Implement max sub array sum solution 1 & 2.

--- sliding_window.js
+++ sliding_window.js
@@ -2,7 +2,23 @@
 Write a function called maxSubarraySum which accepts an array of integers and a number called n.
 The function should calculate the maximum sum of n consecutive elements in the array.
 */
-function maxSubarraySum(arr, n){
+//Solution 1:
+function maxSubarraySum1(arr, n){
+    if(n > arr.length) return null;
+
+    let max = -Infinity;
+    for(let i = 0; i < arr.length - n + 1; i++){
+        let temp = 0;
+        for(let j = 0; j < n; j++){
+            temp += arr[i + j];
+        }
+        if(temp > max) max = temp;
+    }
+    return max;
+}
+
+// Solution 2:
+function maxSubarraySum2(arr, n){
     let maxSum = 0;
     let tempSum = 0;
 
@@ -19,8 +35,8 @@ function maxSubarraySum(arr, n){
     return maxSum;
 }
 
-console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)); // 10
-console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4)); // 17
-console.log(maxSubarraySum([4, 2, 1, 6], 1)); // 6
-console.log(maxSubarraySum([4, 2, 1, 6, 2], 4)); // 13
-console.log(maxSubarraySum([], 4)); // null
+console.log(maxSubarraySum2([1, 2, 5, 2, 8, 1, 5], 2)); // 10
+console.log(maxSubarraySum2([1, 2, 5, 2, 8, 1, 5], 4)); // 17
+console.log(maxSubarraySum2([4, 2, 1, 6], 1)); // 6
+console.log(maxSubarraySum2([4, 2, 1, 6, 2], 4)); // 13
+console.log(maxSubarraySum2([], 4)); // null
