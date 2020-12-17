d5d8547697cc9f292b5e7aac7db3c5bbfc6baabb
Elmehdi Aitbrahim <eaitbrahim@gmail.com>
2020-12-17 14:23:12 -0500

    Implement multiple pointers algo

---
+++ multiple_pointers.js
@@ -0,0 +1,21 @@
+/*
+Creating pointers or values that correspond to an index or position and move towards the beginning,
+end or middle based on a certain condition. Very efficient for solving problems with minimal space complexity as well.
+
+Write a function called sumZero which accepts a sorted array of integers.
+The function should find the first pair where the sum is 0.
+Return an array that includes both values that sum to Zero or undefined if a pair does not exist.
+*/
+
+function sumZero(arr){
+    let left = 0;
+    let right = arr.length -1;
+    while(left < right){
+        let sum = arr[left] + arr[right];
+        if(sum === 0) return [arr[left], arr[right]];
+        else if (sum > 0) right--;
+        else left++;
+    }
+}
+
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])); // [-3, 3]
