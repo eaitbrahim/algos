73def1e2891bc4a637e2eef451ac9082756900ed
Elmehdi Aitbrahim <eaitbrahim@gmail.com>
2020-12-17 16:13:37 -0500

    Implmenet solution 1 for sum zero with time complexity O(n**2) & space complexity O(1)

--- multiple_pointers.js
+++ multiple_pointers.js
@@ -6,7 +6,16 @@ Write a function called sumZero which accepts a sorted array of integers.
 The function should find the first pair where the sum is 0.
 Return an array that includes both values that sum to Zero or undefined if a pair does not exist.
 */
+// Solution 1:
+function sumZero1(arr){
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i + 1; j < arr.length; j++){
+            if(arr[i] + arr[j] === 0) return [arr[i], arr[j]];
+        }
+    }
+}
 
+// Solution 2
 function sumZero(arr){
     let left = 0;
     let right = arr.length -1;
